b19eac6cddcb56de2074e9aff14ee879ae6c0a3b
Brian West <brian@signalwire.com>
2025-05-09 18:35:02 +0000

    update to overcome a bug due to lib change

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from typing import overload
 from flask import Flask, request, jsonify
-from signalwire_ml import SignalWireML
-from signalwire_swaig.core import SWAIG, SWAIGArgument
+from signalwire_swml.swml import SignalWireML
+from signalwire_swaig.swaig import SWAIG, SWAIGArgument
 import os
 import requests
 from dotenv import load_dotenv
@@ -43,7 +43,7 @@ def transfer(target, meta_data_token=None, meta_data=None):
             {"to": target_uri},
         )
     
-    return "Tell the user you are going to transfer the call to whoever they asked for. Do not change languages from the one you are currently using. Do not hangup.", {"SWML": transfer.render(), "transfer": "true"}
+    return "Tell the user you are going to transfer the call to whoever they asked for. Do not change languages from the one you are currently using. Do not hangup.", [{"set_meta_data": meta_data,"SWML": transfer.render(), "transfer": "true"}]
 
 @swaig.endpoint("Send message",
     to=SWAIGArgument("string", "Phone number to send the message to in e.164 format. eg +1234567890"),
@@ -60,7 +60,7 @@ def send_message(to, message, meta_data_token=None, meta_data=None):
                 "body": message,
             }
         )
-        return "Message has been sent.", {"SWML": msg.render()}
+        return "Message has been sent.", [{"SWML": msg.render()}]
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=os.getenv('PORT', 5000), debug=os.getenv('DEBUG', False))
